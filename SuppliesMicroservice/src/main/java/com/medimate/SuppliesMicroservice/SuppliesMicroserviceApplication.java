565c0ad24fbf7b11026ee7e6cf628c32ec6bbd39
unknown <kenanforto@hotmail.com>
2024-03-17 22:31:55 +0100

    Add initial setup for supplies microservice

---
+++ SuppliesMicroservice/src/main/java/com/medimate/SuppliesMicroservice/SuppliesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medimate.SuppliesMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppliesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ SuppliesMicroservice/src/test/java/com/medimate/SuppliesMicroservice/SuppliesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medimate.SuppliesMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppliesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
