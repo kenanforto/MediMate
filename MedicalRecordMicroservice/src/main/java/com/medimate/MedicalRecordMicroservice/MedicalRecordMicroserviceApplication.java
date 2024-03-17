b40e4ddb5d4dc8742408764ebcc0e3293cea441d
unknown <kenanforto@hotmail.com>
2024-03-17 22:28:38 +0100

    Add initial setup for medicalrecord microservice

---
+++ MedicalRecordMicroservice/src/main/java/com/medimate/MedicalRecordMicroservice/MedicalRecordMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medimate.MedicalRecordMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordMicroserviceApplication.class, args);
+	}
+
+}
---
+++ MedicalRecordMicroservice/src/test/java/com/medimate/MedicalRecordMicroservice/MedicalRecordMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medimate.MedicalRecordMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
