f8c1a2b79ea7c057a6591a7afc722edb7c8d473e
unknown <kenanforto@hotmail.com>
2024-03-17 22:32:31 +0100

    Add initial setup for survey microservice

---
+++ SurveyMicroservice/src/main/java/com/medimate/SurveyMicroservice/SurveyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medimate.SurveyMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyMicroserviceApplication.class, args);
+	}
+
+}
---
+++ SurveyMicroservice/src/test/java/com/medimate/SurveyMicroservice/SurveyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medimate.SurveyMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
