cd9ad6ae24b9ca5a09156dcb9e983cdac0f3d50d
unknown <kenanforto@hotmail.com>
2024-03-17 22:17:02 +0100

    Add initial setup for admission microservice

---
+++ AdmissionMicroservice/src/main/java/com/medimate/AdmissionMicroservice/AdmissionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medimate.AdmissionMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ AdmissionMicroservice/src/test/java/com/medimate/AdmissionMicroservice/AdmissionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medimate.AdmissionMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
