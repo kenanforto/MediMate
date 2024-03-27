927014684db0264940a292cb8b28fe5c48196795
Esma Dervisevic <esma.d@ecronix.com>
2024-03-27 10:52:30 +0100

    validation done

--- SuppliesMicroservice/src/main/java/com/medimate/SuppliesMicroservice/viewmodels/SuppliesVM.java
+++ SuppliesMicroservice/src/main/java/com/medimate/SuppliesMicroservice/viewmodels/SuppliesVM.java
@@ -1,10 +1,22 @@
 package com.medimate.SuppliesMicroservice.viewmodels;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class SuppliesVM {
 
+    @NotBlank
+    @NotNull
+    @Pattern(regexp="[A-Za-z]+")
     private String medicationName;
+
+    @NotNull
+    @Min(1)
     private Integer amount;
 
+    @NotNull
     private Integer adminId;
 
     public SuppliesVM(String medicationName, Integer amount, Integer adminId) {
