563aae9affc618415328154cb5ac262b7a4bd5b2
unknown <kenanforto@hotmail.com>
2024-04-03 00:21:07 +0200

    Added eureka

--- UserMicroservice/src/main/java/com/medimate/UserMicroservice/UserMicroserviceApplication.java
+++ UserMicroservice/src/main/java/com/medimate/UserMicroservice/UserMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.medimate.UserMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
