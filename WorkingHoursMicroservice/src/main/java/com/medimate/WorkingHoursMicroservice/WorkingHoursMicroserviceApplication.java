fb2a7edccdfb22e7d16297b5b0adc68c5853f0e6
unknown <kenanforto@hotmail.com>
2024-04-03 00:32:36 +0200

    Added eureka

--- WorkingHoursMicroservice/src/main/java/com/medimate/WorkingHoursMicroservice/WorkingHoursMicroserviceApplication.java
+++ WorkingHoursMicroservice/src/main/java/com/medimate/WorkingHoursMicroservice/WorkingHoursMicroserviceApplication.java
@@ -3,8 +3,10 @@ package com.medimate.WorkingHoursMicroservice;
 import com.medimate.WorkingHoursMicroservice.models.WorkingHours;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WorkingHoursMicroserviceApplication {
 
 	public static void main(String[] args) {
