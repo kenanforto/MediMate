7a6d62d3986ab39eecaea9e2adb7f3315232aa8d
unknown <kenanforto@hotmail.com>
2024-05-21 13:41:24 +0200

    Implementation of system event service

--- MedicalRecordMicroservice/src/main/java/com/medimate/MedicalRecordMicroservice/MedicalRecordMicroserviceApplication.java
+++ MedicalRecordMicroservice/src/main/java/com/medimate/MedicalRecordMicroservice/MedicalRecordMicroserviceApplication.java
@@ -1,8 +1,10 @@
 package com.medimate.MedicalRecordMicroservice;
 
+import com.medimate.MedicalRecordMicroservice.interceptor.SystemEventsInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +13,8 @@ public class MedicalRecordMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedicalRecordMicroserviceApplication.class, args);
 	}
+
+	@Bean
+	public SystemEventsInterceptor customInterceptor(){return new SystemEventsInterceptor();}
 
 }
